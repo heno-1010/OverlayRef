63072236c0438efebde8a50a92606e274aae90d6
heno-1010 <heno-1010@users.noreply.github.com>
2026-05-04 18:12:07 +0900

    feat: display image in window

--- OverlayRef/MainWindow.xaml.cs
+++ OverlayRef/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows;
+﻿using OverlayRef.ViewModels;
+using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media.Imaging;
 
 namespace OverlayRef
 {
@@ -8,9 +10,13 @@ namespace OverlayRef
     /// </summary>
     public partial class MainWindow : Window
     {
+        private MainViewModel _vm;
+
         public MainWindow()
         {
             InitializeComponent();
+            _vm = new MainViewModel();
+            DataContext = _vm;
         }
 
         private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -22,7 +28,35 @@ namespace OverlayRef
         }
         private void OpenImage_Click(object sender, RoutedEventArgs e)
         {
+            var dig = new Microsoft.Win32.OpenFileDialog();
+            dig.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp;*.gif";
+            if(dig.ShowDialog() == true)
+            {
+                var bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.UriSource = new Uri(dig.FileName);
+                bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                bitmap.EndInit();
 
+                _vm.ImageSource = bitmap;
+
+                var maxWidth = SystemParameters.PrimaryScreenWidth * 0.8;
+                var maxHeight = SystemParameters.PrimaryScreenHeight * 0.8;
+
+                double imageWidth = bitmap.PixelWidth;
+                double imageHeight = bitmap.PixelHeight;
+
+                double scale = Math.Min(maxWidth / imageWidth, maxHeight / imageHeight);
+                scale = Math.Min(scale, 1.0);
+
+                this.Width = imageWidth * scale;
+                this.Height = imageHeight * scale;
+
+                this.SizeToContent = SizeToContent.Manual;
+
+                NotImageGrid.Visibility = Visibility.Collapsed;
+                ImageGrid.Visibility = Visibility.Visible;
+            }
         }
 
         private void Close_Click(object sender, RoutedEventArgs e)
---
+++ OverlayRef/ViewModels/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Media.Imaging;
+
+namespace OverlayRef.ViewModels
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        private BitmapImage _imageSource;
+        public BitmapImage ImageSource
+        {
+            get => _imageSource;
+            set
+            {
+                _imageSource = value;
+                OnPropertyChanged(nameof(ImageSource));
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
