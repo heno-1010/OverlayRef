db1f4afa51f8a267116336a7ee9eb65f557224d3
heno-1010 <heno-1010@users.noreply.github.com>
2026-05-04 15:48:50 +0900

    feat:make window draggable

--- OverlayRef/MainWindow.xaml.cs
+++ OverlayRef/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace OverlayRef
 {
@@ -19,6 +11,14 @@ namespace OverlayRef
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ButtonState == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
         }
     }
 }
